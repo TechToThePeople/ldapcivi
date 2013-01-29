5236f3ab9d2d62bf6e09e36ab0293aa1e10ae874
Xavier Dutoit <xavier@tttp.eu>
2013-01-29 18:40:07 +0100

    clearer instruction

--- README.md
+++ README.md
@@ -13,3 +13,19 @@ edit config/yoursite.js with your setting
 Run
 ---
 node server.js yoursite
+
+On the CiviCRM side
+-----------------
+you need to install an extension to provide the api contact.getttpquick
+settings.civicrm.action="getttpquick"
+
+
+Use from Thunderbird
+------------------
+Set up the same ldap.civ.im, port, base DN (SUFFIX in the config) and Bind DB as you have in your config
+for the login, put whatever you want. for the password, put the one on the config
+
+Use from Outlook
+-----------------
+Same as thunderbird, but the login has to be 
+cn=nicolas, dc=example, dc=org (or whatever your bind DN)
--- config/default.js
+++ config/default.js
@@ -1,18 +1,20 @@
 var settings = {}
 
 settings.ldap = {}
-settings.ldap.SUFFIX = '';
-settings.ldap.basedn = "dc=*", // "cn=root" another alternative
-settings.ldap.company = "Example",
+settings.ldap.basedn = "dc=yoursite, dc=org", // "cn=root" another alternative
+settings.ldap.SUFFIX = 'dc=yoursite, dc=org'; //base search. If not defined basedn
+settings.ldap.company = "Your site",
 settings.ldap.port = 1389, // 389 is the ldap port, but needs to run as root (priviledged port 
 settings.ldap.password="you need to override it"
+settings.ldap.enabled=false // you need to remove that line on your copy
 
 settings.civicrm = {}
 settings.civicrm.server ="http://www.example.org"
 settings.civicrm.path = "/sites/all/modules/civicrm/extern/rest.php",
 settings.civicrm.api_key="your api key"
-settings.civicrm.key="yours site key"
+settings.civicrm.key="your site key"
 
-settings.civicrm.action="get"
+settings.civicrm.action="getttpquick"
+//settings.civicrm.action="get" you can use get if you don't install the extension eu.tttp.qlookup 
 
 module.exports = settings;
